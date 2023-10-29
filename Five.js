c0d8626945388cf8986aa827ae9d8d7de6f0258d
Gunjan56 <gangwargunjan9@gmail.com>
2023-10-29 15:35:38 +0530

    Update

--- Five.js
+++ Five.js
@@ -12,3 +12,22 @@ const array1 = [1,2,3,[3,4,[5,6,[7,8]]],8,90];
 let flatArr = array1.flat(3);
 console.log(flatArr);
 
+//every method => every methos in javascript is used to check if all the elements in array pass the test
+//it returns boolean if all elements pass the test otherwise false
+const array2 = [1,3,2,5,6,7,40,50,60];
+let everyMethod = array2.every((element, index, array) =>{
+    return element > 5;
+})
+console.log(everyMethod);
+
+//find() => used to find first element in array that satisfy the given condition
+//if value is not found return undefined
+const array3 = [1,4,6,8,78,14,0];
+const findMethod = array3.find((element) => element>13);
+console.log(findMethod);
+
+//findIndex() = same as find method but in findIndex it return the index value of first element that satisfy the condition else return -1
+const findIndex = array3.findIndex((element) => element > 6);
+console.log(findIndex);
+
+//some() => 
