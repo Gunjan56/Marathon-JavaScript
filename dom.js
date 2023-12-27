c7f001b19c5172929b81423d14f56901541e830c
Gunjan Gangwar <gangwargunjan9@gmail.com>
2023-12-27 14:38:54 +0530

    final code

--- dom.js
+++ dom.js
@@ -392,76 +392,214 @@
 //javaScript Form=>lets learn about js form API
 
 // show a message with a type of the input
-function showMessage(input, message, type) {
-  // get the small element and set the message
-  const msg = input.parentNode.querySelector("small");
-  msg.innerText = message;
-  // update the class for the input
-  input.className = type ? "success" : "error";
-  return type;
-}
+// function showMessage(input, message, type) {
+//   // get the small element and set the message
+//   const msg = input.parentNode.querySelector("small");
+//   msg.innerText = message;
+//   // update the class for the input
+//   input.className = type ? "success" : "error";
+//   return type;
+// }
 
-function showError(input, message) {
-  return showMessage(input, message, false);
-}
+// function showError(input, message) {
+//   return showMessage(input, message, false);
+// }
 
-function showSuccess(input) {
-  return showMessage(input, "", true);
-}
+// function showSuccess(input) {
+//   return showMessage(input, "", true);
+// }
 
-function hasValue(input, message) {
-  if (input.value.trim() === "") {
-    return showError(input, message);
-  }
-  return showSuccess(input);
-}
+// function hasValue(input, message) {
+//   if (input.value.trim() === "") {
+//     return showError(input, message);
+//   }
+//   return showSuccess(input);
+// }
 
-function validateEmail(input, requiredMsg, invalidMsg) {
-  // check if the value is not empty
-  if (!hasValue(input, requiredMsg)) {
-    return false;
-  }
-  // validate email format
-  const emailRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// function validateEmail(input, requiredMsg, invalidMsg) {
+//   // check if the value is not empty
+//   if (!hasValue(input, requiredMsg)) {
+//     return false;
+//   }
+// validate email format
+//   const emailRegex =
+//     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  const email = input.value.trim();
-  if (!emailRegex.test(email)) {
-    return showError(input, invalidMsg);
-  }
-  return true;
-}
+//   const email = input.value.trim();
+//   if (!emailRegex.test(email)) {
+//     return showError(input, invalidMsg);
+//   }
+//   return true;
+// }
 
-const form = document.querySelector("#signup");
+// const form = document.querySelector("#signup");
 
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
+// const NAME_REQUIRED = "Please enter your name";
+// const EMAIL_REQUIRED = "Please enter your email";
+// const EMAIL_INVALID = "Please enter a correct email address format";
 
-form.addEventListener("submit", function (event) {
-  // stop form submission
-  event.preventDefault();
+// form.addEventListener("submit", function (event) {
+// stop form submission
+//   event.preventDefault();
 
-  // validate the form
-  let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-  let emailValid = validateEmail(
-    form.elements["email"],
-    EMAIL_REQUIRED,
-    EMAIL_INVALID
-  );
-  // if valid, submit the form.
-  if (nameValid && emailValid) {
-    alert("only for Practice");
-  }
+//   // validate the form
+//   let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+//   let emailValid = validateEmail(
+//     form.elements["email"],
+//     EMAIL_REQUIRED,
+//     EMAIL_INVALID
+//   );
+//   // if valid, submit the form.
+//   if (nameValid && emailValid) {
+//     alert("only for Practice");
+//   }
+// });
+
+//Radio button=> lets learn how to write the javaScript for radio buttons
+//a group of radio buttons is called radio group
+// const button = document.querySelector("#btn");
+// const radioButtons = document.querySelectorAll("input[name='size']");
+// // let output = document.querySelector("#output");
+
+// button.addEventListener("click", (event) => {
+//   let selectedSize;
+//   for (let radioButton of radioButtons) {
+//     if (radioButton.checked) {
+//       selectedSize = radioButton.value;
+//       break;
+//     }
+//   }
+//   output.innerText = selectedSize
+//     ? `You selected ${selectedSize}`
+//     : `You have not selected any size`;
+// });
+
+//check box=>lets learn how to use js to test if checkbox is checked or not, get the value of selected checkboxes
+//and select unselect all the checkboxes
+// const checked = document.querySelector("#accept");
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", (e) => {
+//   let checkboxes = document.querySelectorAll("input[name='color']:checked");
+//   let values = [];
+//   checkboxes.forEach((checkbox) =>{
+//     values.push(checkbox.value);
+//   });
+//   alert(values);
+// });
+
+// function check(checked = true) {
+//   const checkboxes = document.querySelectorAll("input[name='color']");
+//   checkboxes.forEach((checkbox) => {
+//     checkbox.checked = checked;
+//   });
+// }
+
+// function checkAll() {
+//   check();
+//   this.onclick = uncheckAll;
+// }
+
+// function uncheckAll() {
+//   check(false);
+//   this.onclick = checkAll;
+// }
+// btn.onclick = checkAll;
+
+//select Box=>
+// const btn = document.querySelector("#btn");
+// const sb = document.querySelector("#framework");
+// btn.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   const selectedValues = [].filter
+//     .call(sb.options, (option) => option.selected)
+//     .map((option) => option.text);
+
+//   alert(selectedValues);
+// });
+
+//Add/Remove Options=>lets learn how to dynamically add or remove options from the selected element in js
+//construct a new option using Dom method
+// const btnAdd = document.querySelector("#btnAdd");
+// const btnRemove = document.querySelector("#btnRemove");
+// const listbox = document.querySelector("#list");
+// const framework = document.querySelector("#framework");
+
+// btnAdd.onclick = (e) => {
+//   e.preventDefault();
+
+//   // validate the option
+//   if (framework.value == "") {
+//     alert("Please enter the name.");
+//     return;
+//   }
+//   // create a new option
+//   const option = new Option(framework.value, framework.value);
+//   // add it to the list
+//   listbox.add(option, undefined);
+
+//   // reset the value of the input
+//   framework.value = "";
+//   framework.focus();
+// };
+
+// // remove selected option
+// btnRemove.onclick = (e) => {
+//   e.preventDefault();
+
+//   // save the selected options
+//   let selected = [];
+
+//   for (let i = 0; i < listbox.options.length; i++) {
+//     selected[i] = listbox.options[i].selected;
+//   }
+
+//   // remove all selected option
+//   let index = listbox.options.length;
+//   while (index--) {
+//     if (selected[index]) {
+//       listbox.remove(index);
+//     }
+//   }
+// };
+
+//Removing Items from <select> element conditionally=>lets learn how to use js to remove items from select element based on a condition
+// const btn = document.querySelector(".btn");
+// const select = document.querySelector("#framework");
+// btn.addEventListener("click", (e) => {
+//   for (let i = 0; i < select.options.length; i++) {
+//     const text = select.options[i].text;
+//     if (text.endsWith("js")) {
+//       select.remove(i);
+//       i--;
+//     }
+//   }
+// });
+
+//Handling change event=> the input element fires the change event one it losses focus, the radio button, checkbox, and select elements fire the change event after they have been selected
+// let result = document.querySelector("#result");
+// document.body.addEventListener("change", function (e) {
+//   let target = e.target;
+//   let message;
+
+//   switch (target.id) {
+//     case "html":
+//       message = "The HTML checkbox changed";
+//       break;
+//     case "css":
+//       message = "The CSS checkbox changed";
+//       break;
+//     case "js":
+//       message = "The JavaScript checkbox changed";
+//       break;
+//   }
+//   result.textContent = message;
+// });
+
+//Handling input event=>lets learn about js input event that fires whenever the value of input select and textarea changes
+const message = document.querySelector("#message");
+const result = document.querySelector("#result");
+message.addEventListener("input", function () {
+  result.textContent = this.value;
 });
-
-//Radio button
-
-//check box
-//select Box
-//Add/Remove Options
-//Removing Items from <select> element conditionally
-//Handling change event
-//Handling input event
 
 //Projects in next chapter 😁😀
